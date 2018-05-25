622bfd04b24730319674f8c2d698c2a8b4209b06
Himanshu <himanshu.aseum@gmail.com>
2018-05-25 11:57:12 +0530

    Removed region delegates and added custom code for geo update

--- GeoTargeting/GoogleMapVC.swift
+++ GeoTargeting/GoogleMapVC.swift
@@ -17,6 +17,8 @@ class GoogleMapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelega
     
     let locationManager = CLLocationManager()
     var monitoredRegions: Dictionary<String, Date> = [:]
+    var isInsideLocation = false
+    var indexOfLocation = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,30 +58,30 @@ class GoogleMapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelega
             // we do have authorization
         else if CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
             
+            self.locationManager.startUpdatingLocation()
         }
         DispatchQueue.main.async {
             
         }
-        self.locationManager.startUpdatingLocation()
     }
     
     func setupData() {
         // check if can monitor regions
         if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
             
-//            var bounds = GMSCoordinateBounds()
-//            for dict in arrayLocations {
-//                let latitude = Double((dict as AnyObject).value(forKey: "latitude") as! String)
-//                let longitude = Double((dict as AnyObject).value(forKey: "longitude") as! String)
-//                self.addMoteringPlace(title: (dict as AnyObject).value(forKey: "title") as! String, latitude: latitude!, longitude: longitude!)
-//                bounds = bounds.includingCoordinate(CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!))
-//            }
-//
-//            self.mapView.animate(with: GMSCameraUpdate.fit(bounds, withPadding: 100.0))
+            var bounds = GMSCoordinateBounds()
+            for dict in arrayLocations {
+                let latitude = Double((dict as AnyObject).value(forKey: "latitude") as! String)
+                let longitude = Double((dict as AnyObject).value(forKey: "longitude") as! String)
+                self.addMoteringPlace(title: (dict as AnyObject).value(forKey: "title") as! String, latitude: latitude!, longitude: longitude!)
+                bounds = bounds.includingCoordinate(CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!))
+            }
+
+            self.mapView.animate(with: GMSCameraUpdate.fit(bounds, withPadding: 100.0))
             
-            self.addMoteringPlace(title: "Satadhar", latitude: 23.06620000, longitude: 72.53260000)
-            self.addMoteringPlace(title: "Home", latitude: 23.08149700, longitude: 72.53834800)
-            self.addMoteringPlace(title: "Landmark Honda", latitude: 23.0480852, longitude: 72.5159037)
+//            self.addMoteringPlace(title: "Satadhar", latitude: 23.06620000, longitude: 72.53260000)
+//            self.addMoteringPlace(title: "Home", latitude: 23.08149700, longitude: 72.53834800)
+//            self.addMoteringPlace(title: "Landmark Honda", latitude: 23.0480852, longitude: 72.5159037)
         }
         else {
             print("System can't track regions")
@@ -91,12 +93,12 @@ class GoogleMapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelega
         let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
         let regionRadius = 100.0
         
-        // setup region
-        let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
-                                                                     longitude: coordinate.longitude), radius: regionRadius, identifier: title)
-        region.notifyOnExit = true
-        region.notifyOnEntry = true
-        locationManager.startMonitoring(for: region)
+//        // setup region
+//        let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
+//                                                                     longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+//        region.notifyOnEntry = true
+//        region.notifyOnExit = true
+//        locationManager.startMonitoring(for: region)
         
         let annotation = GMSMarker(position: coordinate)
         annotation.title = "\(title)"
@@ -134,15 +136,20 @@ class GoogleMapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelega
         print(error)
     }
     
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        showAlert("Enter: \(region.identifier)")
-        monitoredRegions[region.identifier] = Date()
-    }
+//    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+//        showAlert("Enter: \(region.identifier)")
+//        monitoredRegions[region.identifier] = Date()
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+//        showAlert("Exit: \(region.identifier)")
+//        monitoredRegions.removeValue(forKey: region.identifier)
+//    }
     
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        showAlert("Exit: \(region.identifier)")
-        monitoredRegions.removeValue(forKey: region.identifier)
-    }
+//    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+//        let camera = GMSCameraPosition.camera(withTarget: (locationManager.location?.coordinate)!, zoom: 17.0, bearing: self.mapView.camera.bearing, viewingAngle: 0)
+//        self.mapView?.animate(to: camera)
+//    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print("Latitude : \(locations[0].coordinate.latitude) Longitude : \(locations[0].coordinate.longitude)")
@@ -150,6 +157,40 @@ class GoogleMapVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelega
         let location = locations.last
         let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
         self.mapView?.animate(to: camera)
+        
+        let isAlreadyInsideLocation = isInsideLocation
+        let previousIndex = indexOfLocation
+        
+        for i in 0..<arrayLocations.count {
+            let dict = arrayLocations[i] as! NSDictionary
+            let coordinate = CLLocation(latitude: Double(dict.value(forKey: "latitude") as! String)!, longitude: Double(dict.value(forKey: "longitude") as! String)!)
+            let distance = location?.distance(from: coordinate)
+            if distance! <= 100.0 {
+                isInsideLocation = true
+                indexOfLocation = i
+                break
+            }
+            else {
+                isInsideLocation = false
+            }
+        }
+        
+        if isAlreadyInsideLocation && isInsideLocation && previousIndex != indexOfLocation {
+            let dictPrev = arrayLocations[previousIndex] as! NSDictionary
+            let dict = arrayLocations[indexOfLocation] as! NSDictionary
+            showAlert("Exit : \(dictPrev.value(forKey: "title") as! String)\nEnter : \(dict.value(forKey: "title") as! String)")
+        }
+        else if !isAlreadyInsideLocation && isInsideLocation {
+            let dict = arrayLocations[indexOfLocation] as! NSDictionary
+            showAlert("Enter : \(dict.value(forKey: "title") as! String)")
+        }
+        else if isAlreadyInsideLocation && !isInsideLocation {
+            let dict = arrayLocations[indexOfLocation] as! NSDictionary
+            showAlert("Exit : \(dict.value(forKey: "title") as! String)")
+        }
+        else {
+            
+        }
         
         //updateRegionsWithLocation(locations[0])
     }
